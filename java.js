9ae35753a3b4acdcd1332942ecd3433fe767b5cf
natascha030203 <natascha030203@gmail.com>
2024-10-04 11:55:20 +0200

    titel ændring

---
+++ java.js
@@ -0,0 +1,18 @@
+document.getElementById("næste").addEventListener("click", function() {
+    document.getElementById("rasmus").innerHTML = "Hvor finder jeg mit skema?";
+});
+
+document.getElementById("næste").addEventListener("click", function() {
+    document.getElementById("svar1").innerHTML = "Timeedit - Moodle";
+});
+document.getElementById("næste").addEventListener("click", function() {
+    document.getElementById("svar2").innerHTML = "Wiseflow";
+});
+
+document.getElementById("næste").addEventListener("click", function() {
+    document.getElementById("svar3").innerHTML = "Outlook";
+});
+
+document.getElementById("næste").addEventListener("click", function() {
+    document.getElementById("svar4").innerHTML = "Zealands hjemmeside";
+});
